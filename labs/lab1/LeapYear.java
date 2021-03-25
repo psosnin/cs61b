0f1f90eb96d9e1cc64e958a8d0e3eecb4a16548c
psosnin <philipsosnin@gmail.com>
2021-03-25 16:49:09 +0000

    project0 complete

--- labs/lab1/LeapYear.java
+++ labs/lab1/LeapYear.java
@@ -6,6 +6,13 @@ public class LeapYear {
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
+    public static boolean isLeapYear(int year) {
+        if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
@@ -13,7 +20,7 @@ public class LeapYear {
             System.out.printf("%d is not a leap year.\n", year);
         }
     }
-
+    
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
--- labs/lab2/Intlist/IntList.java
+++ labs/lab2/Intlist/IntList.java
@@ -82,7 +82,15 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null) {
+            return B;
+        }
+        IntList endA = A.rest;
+        while (endA.rest != null) {
+            endA = endA.rest;
+        }
+        endA.rest = B;
+        return A;
     }
 
     /**
@@ -91,7 +99,19 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null) {
+            return B;
+        }
+        IntList cat = new IntList(A.first, null);
+        IntList ptr = cat;
+        A = A.rest;
+        while (A != null) {
+            ptr.rest = new IntList(A.first, null);
+            ptr = ptr.rest;
+            A = A.rest;
+        }
+
+        return dcatenate(cat, B);
     }
 
 
