7db2665496ce1b60041590686cf467ab14906b1a
psosnin <philipsosnin@gmail.com>
2021-03-31 00:55:31 +0100

    Project 1A: linked list deque completed

--- labs/lab1/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println(5 + "10"); 
-        System.out.println(5 + 10);
-    }
-}
-
-//We use javac HelloWorld.Java to compile then java HelloWorld to run
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,17 +1,17 @@
-public class LinkedListDeque<T>{
+public class LinkedListDeque<T> {
     private int size;
     private DNode sentinel;
 
     private class DNode {
-        public T item;
-        public DNode next;
-        public DNode prev;
+        private T item;
+        private DNode next;
+        private DNode prev;
 
         /*Creates an empty DNode*/
-        public DNode() {}
+        DNode() { }
 
         /*Creates a DNode*/
-        public DNode(T i, DNode n, DNode p){
+        DNode(T i, DNode n, DNode p) {
             item = i;
             next = n;
             prev = p;
@@ -32,47 +32,48 @@ public class LinkedListDeque<T>{
         test.addFirst(10);
         test.addFirst(8);
         test.addLast(12);
-        test.removeFirst();
-        test.removeLast();
+        System.out.println(test.getRecursive(1));
+        System.out.println(test.getRecursive(0));
+        System.out.println(test.getRecursive(13));
     }
 
     /*Creates a deep copy of "other"*/
-    public LinkedListDeque(LinkedListDeque other){
+    public LinkedListDeque(LinkedListDeque other) {
 
     }
 
     /* Adds an item of type T to the front of the deque*/
-    public void addFirst(T item){
+    public void addFirst(T item) {
         size = size + 1;
         sentinel.next.prev = new DNode(item, sentinel.next, sentinel);
         sentinel.next = sentinel.next.prev;
     }
 
     /*Adds an item of type T to the back of the deque*/
-    public void addLast(T item){
+    public void addLast(T item) {
         size = size + 1;
         sentinel.prev.next = new DNode(item, sentinel, sentinel.prev);
         sentinel.prev = sentinel.prev.next;
     }
 
     /*Returns true if deque is empty, false otherwise*/
-    public boolean isEmpty(){
-        if (sentinel.next == sentinel && sentinel.prev == sentinel){
+    public boolean isEmpty() {
+        if (sentinel.next == sentinel && sentinel.prev == sentinel) {
             return true;
         }
         return false;
     }
 
     /*Returns the number of items in the deque*/
-    public int size(){
+    public int size() {
         return size;
     }
 
     /*Prints the items in the deque from first to last, separated bu a space.
      Once all the items have been printed, print out a new line*/
-    public void printDeque(){
+    public void printDeque() {
         DNode ptr = sentinel.next;
-        while (ptr != sentinel){
+        while (ptr != sentinel) {
             System.out.print(ptr.item + " ");
             ptr = ptr.next;
         }
@@ -80,7 +81,7 @@ public class LinkedListDeque<T>{
     }
 
     /*Removes and returns the item at the front of the deque, If no such item exists, return null*/
-    public T removeFirst(){
+    public T removeFirst() {
         if (isEmpty()) {
             return null;
         }
@@ -92,7 +93,7 @@ public class LinkedListDeque<T>{
     }
 
     /*Removes and returns the item at the back of the deque, If no such item exists, return null*/
-    public T removeLast(){
+    public T removeLast() {
         if (isEmpty()) {
             return null;
         }
@@ -105,13 +106,34 @@ public class LinkedListDeque<T>{
 
     /*Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
     If no such item exists, returns null. Must not alter the deque. */
-    public T get(int index){
+    public T get(int index) {
+        if (index < size) {
+            DNode ptr = sentinel.next;
+            while (index > 0) {
+               ptr = ptr.next;
+               index = index - 1;
+            }
+            return ptr.item;
+        }
         return null;
     }
 
     /*Same as get, but uses recursion*/
-    public T getRecursive(int index){
-        return null;
+    public T getRecursive(int index) {
+        if (index >= size) {
+            return null;
+        }
+        else {
+            return getRecursiveHelper(sentinel.next, index);
+        }
     }
 
+    private T getRecursiveHelper(DNode start, int index){
+        if (index == 0) {
+            return start.item;
+        }
+        else {
+            return getRecursiveHelper(start.next, index - 1);
+        }
+    }
 }
